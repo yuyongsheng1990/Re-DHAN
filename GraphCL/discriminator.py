optimize GPU~CUDA: numpy -> tensor. Ensure the correct of gradient and optimizer

GraphCL/discriminator.py
@@ -19,7 +19,7 @@ class Discriminator(nn.Module):
     def forward(self, aug_embed, h_pos, h_neg, device, s_bias1=None, s_bias2=None):
 
         aug_embed, h_pos, h_neg = aug_embed.to(device), h_pos.to(device), h_neg.to(device)
-        aug_x = aug_embed.expand_as(h_pos).to(device)
+        aug_x = aug_embed.expand_as(h_pos)
 
         # Bilinear双向线性映射，将subgraph embedding 与pos embedding对齐；将sub embedding 2与neg embedding对齐。
         # pos对齐，相似度为1，neg为0. 表明是graph-level embedding 的一致性

layers/S2_TripletLoss.py
@@ -86,7 +86,65 @@ def distance_matrix_computation(vectors):
     return distance_matrix
 
 
-# 具体实现三元损失函数triplets_loss，返回某标签下ith元素和jth元素，其最大loss对应的其他标签元素索引
+# # numpy version: 具体实现三元损失函数triplets_loss，返回某标签下ith元素和jth元素，其最大loss对应的其他标签元素索引
+# class FunctionNegativeTripletSelector(TripletSelector):
+#     '''
+#     For each positive pair, takes the hardest negative sample (with the greatest triplet loss value) to create a triplet
+#     Margin should match the margin used in triplet loss.
+#     negative_selection_fn should take array of loss_values for a given anchor-positive pair and all negative samples
+#     and return a negative index for that pair
+#     '''
+#
+#     def __init__(self, margin, negative_selection_fn, cpu=True):
+#         super(FunctionNegativeTripletSelector, self).__init__()
+#         self.cpu = cpu
+#         self.margin = margin
+#         self.negative_selection_fn = negative_selection_fn  # 返回loss_values最大元素值的index的selector
+#
+#     def get_triplets(self, embeddings, labels):  # (100, 192); target, (100, )
+#         if self.cpu:
+#             embeddings = embeddings.cpu()
+#         distance_matrix = distance_matrix_computation(embeddings)  # pre embedding计算distance matrix (100, 100)
+#         distance_matrix = distance_matrix.cpu()
+#
+#         labels = labels.cpu().data.numpy()  # 100
+#         triplets = []
+#
+#         # embedding计算的distance matrix与labels计算loss，取最大loss_index
+#         # 对于每个标签label
+#         for label in set(labels):
+#             label_mask = (labels == label)  # numpy array, (100,), ([True, False, True, True])
+#             label_indices = np.where(label_mask)[0]  # 同一标签索引, label_index, (3, ) array([0, 2, 3], dtype=int64)
+#             if len(label_indices) < 2:
+#                 continue
+#             negative_indices = np.where(np.logical_not(label_mask))[  # (97, )
+#                 0]  # 其他标签索引, not_label_index, array([1], dtype=int64)
+#             anchor_pos_list = list(combinations(label_indices, 2))  # 2个元素的标签索引组合, list: 3, [(23, 66), (23, 79), (66, 79)]
+#             anchor_pos_list = np.array(anchor_pos_list)  # 转换成np.array才能进行slice切片操作, (3, 2)
+#
+#             # 按照anchor_positive index从距离矩阵中抽取distance；0-index，array([0, 0, 2]);
+#             # 从distance_matrix中提取标签label的i-element与j-element距离。
+#             anchor_p_distances = distance_matrix[
+#                 anchor_pos_list[:, 0], anchor_pos_list[:, 1]]  # 同一label不同位置之间的距离distance, (3, ),tensor([-1.1761,-0.8381,0.0099])
+#             for anchor_positive, ap_distance in zip(anchor_pos_list, anchor_p_distances):  # 每个标签下，元素组合、元素距离
+#                 # 0表示ith元素到各个其他标签元素的距离。
+#                 # 同一标签下(ith,jth)距离 - ith元素到其他标签元素的距离 + self.margin边际收益
+#                 loss_values = ap_distance - distance_matrix[  # loss_values, (97, ); ap_distance是同一label第一个位置到第二个位置的距离
+#                     torch.LongTensor(np.array([anchor_positive[0]])), torch.LongTensor(negative_indices)] + self.margin  # anchor_positive, (2, ) [23, 66]; label第一个位置到其他label位置的距离
+#                 loss_values = loss_values.data.cpu().numpy()  # (97, )
+#                 hard_neg_max_index = self.negative_selection_fn(loss_values)  # hard返回最大loss的索引, 4
+#                 if hard_neg_max_index is not None:  # if 最大loss值非空
+#                     hard_negative = negative_indices[hard_neg_max_index]  # 返回最大loss对应的negative label 位置
+#                     # 对于谋标签下ith元素和jth元素，其最大loss对应的其他标签元素索引
+#                     triplets.append([anchor_positive[0], anchor_positive[1], hard_negative])  # positive label位置, negative label位置
+#
+#         if len(triplets) == 0:
+#             triplets.append([anchor_positive[0], anchor_positive[1], negative_indices[0]])
+#
+#         triplets = np.array(triplets)  # (179, 3)
+#         return torch.LongTensor(triplets)
+
+# tensor version: 具体实现三元损失函数triplets_loss，返回某标签下ith元素和jth元素，其最大loss对应的其他标签元素索引
 class FunctionNegativeTripletSelector(TripletSelector):
     '''
     For each positive pair, takes the hardest negative sample (with the greatest triplet loss value) to create a triplet
@@ -95,7 +153,7 @@ class FunctionNegativeTripletSelector(TripletSelector):
     and return a negative index for that pair
     '''
 
-    def __init__(self, margin, negative_selection_fn, cpu=True):
+    def __init__(self, margin, negative_selection_fn, cpu=False):
         super(FunctionNegativeTripletSelector, self).__init__()
         self.cpu = cpu
         self.margin = margin
@@ -105,22 +163,22 @@ class FunctionNegativeTripletSelector(TripletSelector):
         if self.cpu:
             embeddings = embeddings.cpu()
         distance_matrix = distance_matrix_computation(embeddings)  # pre embedding计算distance matrix (100, 100)
-        distance_matrix = distance_matrix.cpu()
+        distance_matrix = distance_matrix
 
-        labels = labels.cpu().data.numpy()  # 100
+        # labels = labels.cpu().data.numpy()  # 100
         triplets = []
 
         # embedding计算的distance matrix与labels计算loss，取最大loss_index
         # 对于每个标签label
         for label in set(labels):
             label_mask = (labels == label)  # numpy array, (100,), ([True, False, True, True])
-            label_indices = np.where(label_mask)[0]  # 同一标签索引, label_index, (3, ) array([0, 2, 3], dtype=int64)
+            label_indices = torch.where(label_mask)[0]  # 同一标签索引, label_index, (3, ) array([0, 2, 3], dtype=int64)
             if len(label_indices) < 2:
                 continue
-            negative_indices = np.where(np.logical_not(label_mask))[  # (97, )
+            negative_indices = torch.where(torch.logical_not(label_mask))[  # (97, )
                 0]  # 其他标签索引, not_label_index, array([1], dtype=int64)
             anchor_pos_list = list(combinations(label_indices, 2))  # 2个元素的标签索引组合, list: 3, [(23, 66), (23, 79), (66, 79)]
-            anchor_pos_list = np.array(anchor_pos_list)  # 转换成np.array才能进行slice切片操作, (3, 2)
+            anchor_pos_list = torch.asarray(anchor_pos_list)  # 转换成np.array才能进行slice切片操作, (3, 2)
 
             # 按照anchor_positive index从距离矩阵中抽取distance；0-index，array([0, 0, 2]);
             # 从distance_matrix中提取标签label的i-element与j-element距离。
@@ -130,8 +188,8 @@ class FunctionNegativeTripletSelector(TripletSelector):
                 # 0表示ith元素到各个其他标签元素的距离。
                 # 同一标签下(ith,jth)距离 - ith元素到其他标签元素的距离 + self.margin边际收益
                 loss_values = ap_distance - distance_matrix[  # loss_values, (97, ); ap_distance是同一label第一个位置到第二个位置的距离
-                    torch.LongTensor(np.array([anchor_positive[0]])), torch.LongTensor(negative_indices)] + self.margin  # anchor_positive, (2, ) [23, 66]; label第一个位置到其他label位置的距离
-                loss_values = loss_values.data.cpu().numpy()  # (97, )
+                    torch.LongTensor(torch.asarray([anchor_positive[0]])), torch.LongTensor(negative_indices)] + self.margin  # anchor_positive, (2, ) [23, 66]; label第一个位置到其他label位置的距离
+                # loss_values = loss_values.data.cpu().numpy()  # (97, )
                 hard_neg_max_index = self.negative_selection_fn(loss_values)  # hard返回最大loss的索引, 4
                 if hard_neg_max_index is not None:  # if 最大loss值非空
                     hard_negative = negative_indices[hard_neg_max_index]  # 返回最大loss对应的negative label 位置
@@ -141,7 +199,7 @@ class FunctionNegativeTripletSelector(TripletSelector):
         if len(triplets) == 0:
             triplets.append([anchor_positive[0], anchor_positive[1], negative_indices[0]])
 
-        triplets = np.array(triplets)  # (179, 3)
+        triplets = torch.asarray(triplets)  # (179, 3)
         return torch.LongTensor(triplets)
 
 # 具体N_pair loss，一个正例，N-1个负例，i-th到其他标签的距离，计算量2N
@@ -287,9 +345,14 @@ def random_hard_negative(loss_values):
     hard_negatives = np.where(loss_values > 0)[0]
     return np.random.choice(hard_negatives) if len(hard_negatives) > 0 else None  # np.random.choice函数默认随机抽取一个数
 
-# hardest,抽取最大的一个数
+# # numpy version: hardest,抽取最大的一个数
+# def hardest_negative(loss_values):
+#     hard_negative = np.argmax(loss_values)
+#     return hard_negative if loss_values[hard_negative] > 0 else None
+
+# tensor version: hardest,抽取最大的一个数
 def hardest_negative(loss_values):
-    hard_negative = np.argmax(loss_values)
+    hard_negative = torch.argmax(loss_values)
     return hard_negative if loss_values[hard_negative] > 0 else None
 
 # 硬三元损失函数

models/Attn_Head.py
@@ -32,8 +32,9 @@ class Attn_Head(nn.Module):
             seq = self.feat_dropout(x)  # 以rate置0
             seq = seq.float()
         # reshape x and bias_mx for nn.Conv1d
-        seq = torch.transpose(seq[np.newaxis], 2, 1).to(device)  # (1, 37, 100)
-        bias_mx = bias_mx[np.newaxis]  # (1, 100, 100)
+        seq = torch.unsqueeze(seq, dim=0)
+        seq = torch.transpose(seq, 2, 1)  # (1, 37, 100)
+        bias_mx = torch.unsqueeze(bias_mx, dim=0)  # (1, 100, 100)
         seq_fts = self.conv1(seq)  # x*Wv=v, 一维卷积操作, out: (1, 16, 100)
 
         f_1 = self.conv2_1(seq_fts)  # x*Wq=q,(1, 1, 100)
@@ -55,15 +56,25 @@ class Attn_Head(nn.Module):
         else:
             return self.activation(ret)  # activation
 
-# time exponential decay formula
+# # numpy version: time exponential decay formula
+# def time_decay_weight(vectors, time_lambda):  # 衰减参数 lambda
+#     # torch 转换为numpy array
+#     vectors = torch.squeeze(vectors.cpu().t()).numpy()  # (1,100)
+#     # 计算每两个元素相减的绝对值并形成矩阵
+#     diff_matrix = np.abs(np.subtract.outer(vectors, vectors))
+#     time_weight_mx = np.exp(diff_matrix * (-time_lambda))  # 不要用softmax，它会将差异抹平！！！
+#     # time_weight_mx = F.softmax(torch.from_numpy(time_matrix), dim=1)  # dim=1, 在行上进行softmax; dim=0, 在列上进行softmax
+#     return torch.from_numpy(time_weight_mx)
+
+# tensor version: time exponential decay formula
 def time_decay_weight(vectors, time_lambda):  # 衰减参数 lambda
-    # torch 转换为numpy array
-    vectors = torch.squeeze(vectors.cpu().t()).numpy()  # (1,100)
+    # torch 转换为 array
+    vectors = torch.squeeze(vectors.t())  # (1,100) -> (100,)
     # 计算每两个元素相减的绝对值并形成矩阵
-    diff_matrix = np.abs(np.subtract.outer(vectors, vectors))
-    time_weight_mx = np.exp(diff_matrix * (-time_lambda))  # 不要用softmax，它会将差异抹平！！！
+    diff_matrix = torch.abs(vectors.unsqueeze(0) - vectors.unsqueeze(1))
+    time_weight_mx = torch.exp(diff_matrix * (-time_lambda))  # 不要用softmax，它会将差异抹平！！！
     # time_weight_mx = F.softmax(torch.from_numpy(time_matrix), dim=1)  # dim=1, 在行上进行softmax; dim=0, 在列上进行softmax
-    return torch.from_numpy(time_weight_mx)
+    return time_weight_mx
 
 # Temporal node attention
 class Temporal_Attn_Head(nn.Module):
@@ -88,8 +99,9 @@ class Temporal_Attn_Head(nn.Module):
             seq = self.feat_dropout(x)  # 以rate置0
             seq = seq.float()
         # reshape x and bias_mx for nn.Conv1d,
-        seq = torch.transpose(seq[np.newaxis], 2, 1).to(device)  # (1, 16, 100)
-        bias_mx = bias_mx[np.newaxis].to(device)  # (1, 100, 100)
+        seq = torch.unsqueeze(seq, dim=0)
+        seq = torch.transpose(seq, 2, 1)  # (1, 16, 100)
+        bias_mx = torch.unsqueeze(bias_mx, dim=0).to(device)  # (1, 100, 100)
         seq_fts = self.conv1(seq)  # x*Wv=v, 一维卷积操作, out: (1, 8, 100)
 
         f_1 = self.conv2_1(seq_fts)  # x*Wq=q,(1, 1, 100)
@@ -184,7 +196,7 @@ class SimpleAttnLayer(nn.Module):
         nn.init.zeros_(self.b_omega)
         nn.init.xavier_uniform_(self.u_omega)
 
-    def forward(self, x, RL_thresholds):  # (100,2,64); 将RL sampleing weight 加入到meta-path importance weight
+    def forward(self, x, device, RL_thresholds):  # (100,2,64); 将RL sampleing weight 加入到meta-path importance weight
         '''
         这是一个点积dot-product attention
         inputs: tensor, (3025, 64)
@@ -193,13 +205,13 @@ class SimpleAttnLayer(nn.Module):
         if isinstance(x, tuple):
             # In case of Bi-RNN, concatenate the forward and the backward RNN outputs.
             inputs = torch.concat(x, 2)  # 表示在shape第2个维度上拼接
-
+        x = x.to(device)
         v = self.tanh(torch.matmul(x, self.w_omega) + self.b_omega)  # (100,2,128) 作为attention q
         vu = torch.matmul(v, self.u_omega)  # (100,2,1) qk相乘得一维相似度向量
         alphas = self.softmax(vu)
         # 在meta-path aggregation weight上加入meta-path, 需要保持shape一致
-        # tensor_rl = RL_thresholds  # (3, 1)
-        # alphas = torch.add(alphas, tensor_rl)  # (100, 3, 1)
+        tensor_rl = RL_thresholds  # (3, 1)
+        alphas = torch.add(alphas, tensor_rl)  # (100, 3, 1)
 
         output = torch.sum(x * alphas.reshape(alphas.shape[0],-1,1), dim=1)  # (100,2,64)*(100,1,2) -> (100,64)
 

models/HeteGAT_multi.py
@@ -20,7 +20,7 @@ class HeteGAT_multi(nn.Module):
                               activation=nn.ELU(), residual=args.residual)
 
     '''
-    def __init__(self, feature_size, nb_classes, nb_nodes, attn_drop, feat_drop, hid_dim, out_dim, time_lambda,
+    def __init__(self, feature_size, nb_classes, nb_nodes, attn_drop, feat_drop, hid_dim, out_dim,
                  bias_mx_len, hid_units, n_heads, activation=nn.ELU()):
         super(HeteGAT_multi, self).__init__()
         self.feature_size = feature_size  # 302
@@ -30,7 +30,6 @@ class HeteGAT_multi(nn.Module):
         self.feat_drop = feat_drop  # 0.0
         self.hid_dim = hid_dim  # 128
         self.out_dim = out_dim  # 64
-        self.time_lambda = time_lambda  # -0.2
         self.bias_mx_len = bias_mx_len  # list:3
         self.hid_units = hid_units  # [8]
         self.n_heads = n_heads  # [8,1]

models/HeteGAT_multi_RL.py
@@ -5,6 +5,7 @@
 # @Project : HAN_torch
 # @Description :
 
+import gc
 import numpy as np
 import scipy.sparse as sp
 import torch
@@ -105,11 +106,11 @@ class HeteGAT_multi_RL(nn.Module):
             layers.append(nn.Sequential(*list(m for m in attn_list)))
         return nn.Sequential(*list(m for m in layers))
 
-    def forward(self, features_list, biases_mat_list, batch_node_list, adjs, n_ids, device, RL_thresholds):
+    def forward(self, features, biases_mat_list, batch_nodes, adjs, n_ids, device, RL_thresholds):
         embed_list = []
 
         # multi-head attention in a hierarchical manner
-        for i, (features, biases) in enumerate(zip(features_list, biases_mat_list)):
+        for i, (biases) in enumerate(biases_mat_list):
             features, biases = features.to(device), biases.to(device)
             attns = []
             '''
@@ -118,7 +119,7 @@ class HeteGAT_multi_RL(nn.Module):
             edge_index=tensor([[]]), e_id=None, size=(137,129); edge_index=tensor([[]]), e_id=None, size=(129, 100)
             edge_index=tensor([[]]), e_id=None, size=(198,152); edge_index=tensor([[]]), e_id=None, size=(152, 100)
             '''
-            batch_nodes = batch_node_list[i].to(device)
+            batch_nodes = batch_nodes.to(device)
             # -----------------1-layer MLP------------------------------------------------
             mlp_features = self.mlp(features[n_ids[i]])
 
@@ -129,7 +130,7 @@ class HeteGAT_multi_RL(nn.Module):
             feature_embedding = self.intra_aggs[i](mlp_features, adjs[i], device)  # (100,128)
             # feature_embedding = self.intra_aggs[i](features[n_ids[i]], adjs[i], device)  # (100,128)
             batch_time = features[batch_nodes][:, -2:-1] * 10000  # (100, 1)  # 恢复成days representation
-            batch_time = batch_time.to(device)
+            # batch_time = batch_time.to(device)
             batch_bias = biases[batch_nodes][:, batch_nodes]  # (100, 100)
 
             # -----------------normalization--------------------------------
@@ -197,12 +198,14 @@ class HeteGAT_multi_RL(nn.Module):
             # feature_embedding = self.linear(F.elu(feature_embedding))
 
             embed_list.append(torch.unsqueeze(feature_embedding, dim=1))
-
+            del feature_embedding
+            gc.collect()
         multi_embed = torch.cat(embed_list, dim=1)   # tensor, (100, 3, 64)
         # simple attention 合并多个meta-based homo-graph embedding
-        final_embed, att_val = self.simpleAttnLayer(multi_embed, RL_thresholds)  # (100, 64)
+        final_embed, att_val = self.simpleAttnLayer(multi_embed, device, RL_thresholds)  # (100, 64)
         # final_embed = torch.mul(multi_embed, RL_thresholds).reshape(len(batch_nodes), -1)
-
+        del multi_embed
+        gc.collect()
         # out = []
         # # 添加一个全连接层做预测(final_embedding, prediction) -> (100, 3)
         # out.append(self.fc(final_embed))

utils/S3_clustering.py
@@ -27,7 +27,7 @@ def run_kmeans(extract_features, extract_labels, indices, isoPath=None):
         # Remove isolated points
         temp = torch.load(isoPath)
         temp = temp.cpu().detach().numpy()  # detach()阻断反向传播，返回值为tensor；numpy()将tensor转换为numpy
-        non_isolated_index = list(np.where(temp != 1)[0])  # np.where返回符合条件元素的索引index
+        non_isolated_index = list(torch.where(temp != 1)[0])  # np.where返回符合条件元素的索引index
         indices = intersection(indices, non_isolated_index)  # 取交集
     # Extract labels
     extract_labels = extract_labels.cpu().numpy()
@@ -67,7 +67,7 @@ def run_dbscan(extract_features, extract_labels, indices, save_path, former_save
         # Remove isolated points
         temp = torch.load(isoPath)
         temp = temp.cpu().detach().numpy()  # detach()阻断反向传播，返回值为tensor；numpy()将tensor转换为numpy
-        non_isolated_index = list(np.where(temp != 1)[0])  # np.where返回符合条件元素的索引index
+        non_isolated_index = list(torch.where(temp != 1)[0])  # np.where返回符合条件元素的索引index
         indices = intersection(indices, non_isolated_index)  # 取交集
     # former embeddings
     former_embeddings = torch.load(former_save_path + '/final_embeddings.pt')